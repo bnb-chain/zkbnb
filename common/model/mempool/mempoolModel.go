14246dfae38f308a761c9a3cc32bc98a7b4d451b
SherLzp <lzp.sher@gmail.com>
2022-04-17 20:51:36 +0800

    [Sher] add copyright

--- common/model/mempool/mempoolModel.go
+++ common/model/mempool/mempoolModel.go
@@ -1,3 +1,20 @@
+/*
+ * Copyright © 2021 Zecrey Protocol
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package mempool
 
 import (
@@ -567,8 +584,8 @@ func (m *defaultMempoolModel) GetMempoolTxsListByL2BlockHeight(blockHeight int64
 	Description: used for l1Amount globalmap to compute latest l1 amount
 */
 func (m *defaultMempoolModel) GetL1MempoolTx(chainId int64, assetId int64) (mempoolTxs []*MempoolTx, err error) {
-	dbTx := m.DB.Table(m.table).Where("status = ? and chain_id = ? and asset_a_id = ? and (tx_type = ? or tx_type = ? or tx_type = ?)",
-		PendingTxStatus, chainId, assetId, commonTx.TxTypeDeposit, commonTx.TxTypeLock, commonTx.TxTypeWithdraw).Find(&mempoolTxs)
+	dbTx := m.DB.Table(m.table).Where("status = ? and chain_id = ? and asset_a_id = ? and (tx_type = ? or tx_type = ?)",
+		PendingTxStatus, chainId, assetId, commonTx.TxTypeDeposit, commonTx.TxTypeWithdraw).Find(&mempoolTxs)
 	if dbTx.Error != nil {
 		logx.Errorf("[mempool.GetL1MempoolTx] %s", dbTx.Error)
 		return nil, dbTx.Error
--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/zecrey-labs/zecrey-legend
 
-go 1.17
+go 1.16
+
+require (
+	github.com/zeromicro/go-zero v1.3.2
+	gorm.io/gorm v1.23.4
+)
+
+require (
+	github.com/consensys/gnark-crypto v0.7.0
+	github.com/google/uuid v1.3.0
+	github.com/zecrey-labs/zecrey-crypto v0.0.26
+	github.com/zeromicro/go-zero/tools/goctl v1.3.4
+)
+
+replace github.com/zecrey-labs/zecrey-crypto => github.com/zecrey-labs/zecrey-crypto v0.0.26
