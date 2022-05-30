5aa7279d3d7bb3e393eb9896787b8a045176cfbd
Neo Hugh <neohugh.cn@gmail.com>
2022-05-30 14:23:54 +0800

    update gettxbyhashlogic

--- service/api/app/internal/logic/transaction/gettxbyhashlogic.go
+++ service/api/app/internal/logic/transaction/gettxbyhashlogic.go
@@ -2,29 +2,105 @@ package transaction
 
 import (
 	"context"
-
+	"fmt"
+	blockModel "github.com/zecrey-labs/zecrey-legend/common/model/block"
+	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/repo/block"
+	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/repo/mempool"
 	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/svc"
 	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/types"
-
 	"github.com/zeromicro/go-zero/core/logx"
+	"strconv"
 )
 
 type GetTxByHashLogic struct {
 	logx.Logger
-	ctx    context.Context
-	svcCtx *svc.ServiceContext
+	ctx     context.Context
+	svcCtx  *svc.ServiceContext
+	mempool mempool.Mempool
+	block   block.Block
 }
 
 func NewGetTxByHashLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetTxByHashLogic {
 	return &GetTxByHashLogic{
-		Logger: logx.WithContext(ctx),
-		ctx:    ctx,
-		svcCtx: svcCtx,
+		Logger:  logx.WithContext(ctx),
+		ctx:     ctx,
+		svcCtx:  svcCtx,
+		mempool: mempool.New(svcCtx.Config),
+		block:   block.New(svcCtx.Config),
+	}
+}
+
+func packGetTxByHashResp(tx types.Tx, committedAt int64, verifiedAt int64, executedAt int64) (res *types.RespGetTxByHash) {
+	return &types.RespGetTxByHash{
+		Tx:          tx,
+		CommittedAt: committedAt,
+		VerifiedAt:  verifiedAt,
+		ExecutedAt:  executedAt,
 	}
 }
 
 func (l *GetTxByHashLogic) GetTxByHash(req *types.ReqGetTxByHash) (resp *types.RespGetTxByHash, err error) {
-	// todo: add your logic here and delete this line
+	//	err := utils.CheckRequestParam(utils.TypeHash, reflect.ValueOf(req.TxHash))
 
-	return
+	txMemppol, err := l.mempool.GetMempoolTxByTxHash(req.TxHash)
+	if err != nil {
+		if err != mempool.ErrNotFound {
+			errInfo := fmt.Sprintf("[appService.transaction.GetTxByHash]<=>[MempoolModel.GetMempoolTxByTxHash] %s", err.Error())
+			logx.Error(errInfo)
+			return packGetTxByHashResp(types.Tx{}, 0, 0, 0), nil
+		}
+	}
+	txDetails := make([]*types.TxDetail, 0)
+	for _, w := range txMemppol.MempoolDetails {
+		txDetails = append(txDetails, &types.TxDetail{
+			AssetId:      int(w.AssetId),
+			AssetType:    int(w.AssetType),
+			AccountIndex: int32(w.AccountIndex),
+			AccountName:  w.AccountName,
+			// #Todo: add balance， but from where?
+			AccountBalance: "0",
+			AccountDelta:   w.BalanceDelta,
+		})
+	}
+	block, err := l.block.GetBlockByBlockHeight(txMemppol.L2BlockHeight)
+	blockStatusInfo := &blockModel.BlockStatusInfo{
+		BlockStatus: block.BlockStatus,
+		CommittedAt: block.CommittedAt,
+		VerifiedAt:  block.VerifiedAt,
+	}
+	// Todo: update blockstatus to cache, but not sure if the whole block shall be inserted
+	//err = l.svcCtx.BlockModel.UpdateBlockStatusCacheByBlockHeight(tx.BlockHeight, blockStatusInfo)
+	//if err != nil {
+	//	errInfo := fmt.Sprintf("[appService.tx.GetTxByHash]<=>[BlockModel.UpdateBlockStatusCacheByBlockHeight] %s", err.Error())
+	//	logx.Error(errInfo)
+	//	return packGetTxByHashResp(types.FailStatus, "fail", errInfo, respResult), nil
+	//}
+	gasFee, _ := strconv.Atoi(txMemppol.GasFee)
+	txAmount, _ := strconv.Atoi(txMemppol.TxAmount)
+	return packGetTxByHashResp(types.Tx{
+		TxHash:        txMemppol.TxHash,
+		TxType:        uint8(txMemppol.TxType),
+		GasFee:        int64(gasFee),
+		GasFeeAssetId: uint16(txMemppol.GasFeeAssetId),
+		TxStatus:      txMemppol.Status,
+		BlockHeight:   int(txMemppol.L2BlockHeight),
+		BlockStatus:   int(blockStatusInfo.BlockStatus),
+
+		// Todo: where is this field come from
+		BlockId: 0,
+
+		AssetAId:      int(txMemppol.AssetAId),
+		AssetBId:      int(txMemppol.AssetBId),
+		TxAmount:      txAmount,
+		TxDetails:     txDetails,
+		NativeAddress: txMemppol.NativeAddress,
+
+		//Todo: remove all chain ID
+		ChainId: 0,
+
+		CreatedAt: txMemppol.CreatedAt.UnixNano() / 1e6,
+		Memo:      txMemppol.Memo,
+
+		// Todo: where is executedAt field from?
+	}, blockStatusInfo.CommittedAt, blockStatusInfo.VerifiedAt, 0), nil
 }
