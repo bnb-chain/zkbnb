bf72375663b32fe1983032b90a1f791043c055e7
yiming <isyiming.zhang@icloud.com>
2022-06-16 20:35:24 +0800

    feat:updata multcache

--- pkg/multcache/api.go
+++ pkg/multcache/api.go
@@ -15,10 +15,32 @@ import (
 // Query function when key does not exist
 type MultCache interface {
 	GetWithSet(ctx context.Context, key string, value interface{}, timeOut uint32,
-		query QueryFunc, arg interface{}) (interface{}, error)
+		query QueryFunc) error
 	Get(ctx context.Context, key string, value interface{}) (interface{}, error)
 	Set(ctx context.Context, key string, value interface{}, timeOut uint32) error
 }
+
+type Book struct {
+	ID   string
+	Name string
+}
+
+// func NewLoadableCache(expiration, cleanupInterval uint32) MultCache {
+// 	gocacheClient := gocache.New(time.Duration(expiration)*time.Minute,
+// 		time.Duration(cleanupInterval)*time.Minute)
+// 	gocacheStore := store.NewGoCache(gocacheClient, nil)
+// 	// goCacheManager := cache.New(gocacheStore)
+// 	// promMetrics := metrics.NewPrometheus("my-amazing-app")
+// 	loadFunction := func(ctx context.Context, key string) (*Book, error) {
+// 		// ... retrieve value from available source
+// 		return &Book{ID: "1", Name: "My test amazing book"}, nil
+// 	}
+// 	cacheManager := cache.NewLoadable(loadFunction, cache.New(gocacheStore))
+
+// 	return &multcache{
+// 		marshal: marshaler.New(cacheManager),
+// 	}
+// }
 
 func NewGoCache(expiration, cleanupInterval uint32) MultCache {
 	gocacheClient := gocache.New(time.Duration(expiration)*time.Minute,
