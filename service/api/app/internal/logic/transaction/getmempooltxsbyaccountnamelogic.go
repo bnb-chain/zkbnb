fbf2901ab7724d85ab17aec3275a474a368f7a07
yiming <isyiming.zhang@icloud.com>
2022-06-24 17:14:22 +0800

    feat:update transaction in app

--- service/api/app/internal/logic/transaction/getmempooltxsbyaccountnamelogic.go
+++ service/api/app/internal/logic/transaction/getmempooltxsbyaccountnamelogic.go
@@ -3,6 +3,10 @@ package transaction
 import (
 	"context"
 
+	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/logic/utils"
+	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/repo/account"
+	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/repo/mempool"
+	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/repo/mempooltxdetail"
 	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/svc"
 	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/types"
 
@@ -11,20 +15,45 @@ import (
 
 type GetmempoolTxsByAccountNameLogic struct {
 	logx.Logger
-	ctx    context.Context
-	svcCtx *svc.ServiceContext
+	ctx             context.Context
+	svcCtx          *svc.ServiceContext
+	account         account.Model
+	memPoolTxDetail mempooltxdetail.Model
+	mempool         mempool.Mempool
 }
 
 func NewGetmempoolTxsByAccountNameLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetmempoolTxsByAccountNameLogic {
 	return &GetmempoolTxsByAccountNameLogic{
-		Logger: logx.WithContext(ctx),
-		ctx:    ctx,
-		svcCtx: svcCtx,
+		Logger:          logx.WithContext(ctx),
+		ctx:             ctx,
+		svcCtx:          svcCtx,
+		account:         account.New(svcCtx),
+		memPoolTxDetail: mempooltxdetail.New(svcCtx),
+		mempool:         mempool.New(svcCtx),
 	}
 }
 
-func (l *GetmempoolTxsByAccountNameLogic) GetmempoolTxsByAccountName(req *types.ReqGetmempoolTxsByAccountName) (resp *types.RespGetmempoolTxsByAccountName, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+func (l *GetmempoolTxsByAccountNameLogic) GetmempoolTxsByAccountName(req *types.ReqGetmempoolTxsByAccountName) (*types.RespGetmempoolTxsByAccountName, error) {
+	resp := &types.RespGetmempoolTxsByAccountName{
+		Txs: make([]*types.Tx, 0),
+	}
+	account, err := l.account.GetAccountByAccountName(l.ctx, req.AccountName)
+	if err != nil {
+		logx.Errorf("[GetAccountByAccountName] err:%v", err)
+		return nil, err
+	}
+	mempoolTxDetails, err := l.memPoolTxDetail.GetMemPoolTxDetailByAccountIndex(l.ctx, int64(account.AccountIndex))
+	if err != nil {
+		logx.Errorf("[GetMemPoolTxDetailByAccountIndex] AccountIndex:%v err:%v", account.AccountIndex, err)
+		return nil, err
+	}
+	for _, d := range mempoolTxDetails {
+		tx, err := l.mempool.GetMempoolTxByTxId(l.ctx, d.TxId)
+		if err != nil {
+			logx.Errorf("[GetMempoolTxByTxID] TxId:%v, err:%v", d.TxId, err)
+			continue
+		}
+		resp.Txs = append(resp.Txs, utils.MempoolTx2Tx(tx))
+	}
+	return resp, nil
 }
--- service/api/app/internal/logic/transaction/getmempooltxslogic.go
+++ service/api/app/internal/logic/transaction/getmempooltxslogic.go
@@ -3,12 +3,11 @@ package transaction
 import (
 	"context"
 
-	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/logic/errcode"
+	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/logic/utils"
 	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/repo/block"
 	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/repo/mempool"
 	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/svc"
 	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/types"
-	"github.com/zecrey-labs/zecrey-legend/utils"
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -30,14 +29,6 @@ func NewGetMempoolTxsLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Get
 	}
 }
 func (l *GetMempoolTxsLogic) GetMempoolTxs(req *types.ReqGetMempoolTxs) (*types.RespGetMempoolTxs, error) {
-	if utils.CheckTypeLimit(req.Limit) {
-		logx.Errorf("[CheckTypeLimit] param:%v", req.Limit)
-		return nil, errcode.ErrInvalidParam
-	}
-	if utils.CheckTypeOffset(req.Offset) {
-		logx.Errorf("[CheckTypeOffset] param:%v", req.Offset)
-		return nil, errcode.ErrInvalidParam
-	}
 	count, err := l.mempool.GetMempoolTxsTotalCount()
 	if err != nil {
 		logx.Errorf("[GetMempoolTxsTotalCount] err:%v", err)
@@ -47,50 +38,13 @@ func (l *GetMempoolTxsLogic) GetMempoolTxs(req *types.ReqGetMempoolTxs) (*types.
 		MempoolTxs: make([]*types.Tx, 0),
 		Total:      uint32(count),
 	}
-	logx.Errorf("[GetMempoolTxsTotalCount] count:%v", count)
-
 	mempoolTxs, err := l.mempool.GetMempoolTxs(int64(req.Limit), int64(req.Offset))
 	if err != nil {
 		logx.Errorf("[GetMempoolTxs] err:%v", err)
 		return nil, err
 	}
 	for _, mempoolTx := range mempoolTxs {
-		txDetails := make([]*types.TxDetail, 0)
-		for _, txDetail := range mempoolTx.MempoolDetails {
-			txDetails = append(txDetails, &types.TxDetail{
-				AssetId:      (txDetail.AssetId),
-				AssetType:    (txDetail.AssetType),
-				AccountIndex: (txDetail.AccountIndex),
-				AccountName:  txDetail.AccountName,
-			})
-		}
-		blockInfo, err := l.block.GetBlockByBlockHeight(l.ctx, mempoolTx.L2BlockHeight)
-		if err != nil {
-			logx.Error("[GetBlockByBlockHeight] err:%v", err)
-			return nil, err
-		}
-		resp.MempoolTxs = append(resp.MempoolTxs, &types.Tx{
-			TxHash:        mempoolTx.TxHash,
-			TxType:        (mempoolTx.TxType),
-			GasFeeAssetId: (mempoolTx.GasFeeAssetId),
-			GasFee:        mempoolTx.GasFee,
-			NftIndex:      (mempoolTx.NftIndex),
-			PairIndex:     (mempoolTx.PairIndex),
-			AssetId:       (mempoolTx.AssetId),
-			TxAmount:      mempoolTx.TxAmount,
-			NativeAddress: mempoolTx.NativeAddress,
-			TxDetails:     txDetails,
-			TxInfo:        mempoolTx.TxInfo,
-			ExtraInfo:     mempoolTx.ExtraInfo,
-			Memo:          mempoolTx.Memo,
-			AccountIndex:  (mempoolTx.AccountIndex),
-			Nonce:         (mempoolTx.Nonce),
-			ExpiredAt:     (mempoolTx.ExpiredAt),
-			BlockHeight:   (mempoolTx.L2BlockHeight),
-			Status:        int64(mempoolTx.Status),
-			CreatedAt:     (mempoolTx.CreatedAt.Unix()),
-			BlockId:       int64(blockInfo.ID),
-		})
+		resp.MempoolTxs = append(resp.MempoolTxs, utils.MempoolTx2Tx(mempoolTx))
 	}
 	return resp, nil
 }
