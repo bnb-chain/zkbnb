59dadf00d543f592436a47e1a21439c556102f5f
zhangwei812 <1733773547@qq.com>
2022-06-30 17:58:33 +0800

    global rpc sendTx classification File name optimization

--- service/rpc/globalRPC/internal/server/globalrpcserver.go
+++ service/rpc/globalRPC/internal/server/globalrpcserver.go
@@ -5,6 +5,7 @@ package server
 
 import (
 	"context"
+	"github.com/zecrey-labs/zecrey-legend/service/rpc/globalRPC/internal/logic/sendRawTypeTx"
 
 	"github.com/zecrey-labs/zecrey-legend/service/rpc/globalRPC/globalRPCProto"
 	"github.com/zecrey-labs/zecrey-legend/service/rpc/globalRPC/internal/logic"
@@ -69,4 +70,49 @@ func (s *GlobalRPCServer) GetNextNonce(ctx context.Context, in *globalRPCProto.R
 func (s *GlobalRPCServer) GetMaxOfferId(ctx context.Context, in *globalRPCProto.ReqGetMaxOfferId) (*globalRPCProto.RespGetMaxOfferId, error) {
 	l := logic.NewGetMaxOfferIdLogic(ctx, s.svcCtx)
 	return l.GetMaxOfferId(in)
+}
+
+func (s *GlobalRPCServer) SendAddLiquidityTx(ctx context.Context, in *globalRPCProto.ReqSendTxByRawInfo) (*globalRPCProto.RespSendTx, error) {
+	l := sendRawTypeTx.NewSendAddLiquidityTxLogic(ctx, s.svcCtx)
+	return l.SendAddLiquidityTx(in)
+}
+
+func (s *GlobalRPCServer) SendAtomicMatchTx(ctx context.Context, in *globalRPCProto.ReqSendTxByRawInfo) (*globalRPCProto.RespSendTx, error) {
+	l := sendRawTypeTx.NewSendAtomicMatchTxLogic(ctx, s.svcCtx)
+	return l.SendAtomicMatchTx(in)
+}
+
+func (s *GlobalRPCServer) SendCancelOfferTx(ctx context.Context, in *globalRPCProto.ReqSendTxByRawInfo) (*globalRPCProto.RespSendTx, error) {
+	l := sendRawTypeTx.NewSendCancelOfferTxLogic(ctx, s.svcCtx)
+	return l.SendCancelOfferTx(in)
+}
+
+func (s *GlobalRPCServer) SendRemoveLiquidityTx(ctx context.Context, in *globalRPCProto.ReqSendTxByRawInfo) (*globalRPCProto.RespSendTx, error) {
+	l := sendRawTypeTx.NewSendRemoveLiquidityTxLogic(ctx, s.svcCtx)
+	return l.SendRemoveLiquidityTx(in)
+}
+
+func (s *GlobalRPCServer) SendSwapTx(ctx context.Context, in *globalRPCProto.ReqSendTxByRawInfo) (*globalRPCProto.RespSendTx, error) {
+	l := sendRawTypeTx.NewSendSwapTxLogic(ctx, s.svcCtx)
+	return l.SendSwapTx(in)
+}
+
+func (s *GlobalRPCServer) SendTransferNftTx(ctx context.Context, in *globalRPCProto.ReqSendTxByRawInfo) (*globalRPCProto.RespSendTx, error) {
+	l := sendRawTypeTx.NewSendTransferNftTxLogic(ctx, s.svcCtx)
+	return l.SendTransferNftTx(in)
+}
+
+func (s *GlobalRPCServer) SendTransferTx(ctx context.Context, in *globalRPCProto.ReqSendTxByRawInfo) (*globalRPCProto.RespSendTx, error) {
+	l := sendRawTypeTx.NewSendTransferTxLogic(ctx, s.svcCtx)
+	return l.SendTransferTx(in)
+}
+
+func (s *GlobalRPCServer) SendWithdrawNftTx(ctx context.Context, in *globalRPCProto.ReqSendTxByRawInfo) (*globalRPCProto.RespSendTx, error) {
+	l := sendRawTypeTx.NewSendWithdrawNftTxLogic(ctx, s.svcCtx)
+	return l.SendWithdrawNftTx(in)
+}
+
+func (s *GlobalRPCServer) SendWithdrawTx(ctx context.Context, in *globalRPCProto.ReqSendTxByRawInfo) (*globalRPCProto.RespSendTx, error) {
+	l := sendRawTypeTx.NewSendWithdrawTxLogic(ctx, s.svcCtx)
+	return l.SendWithdrawTx(in)
 }
