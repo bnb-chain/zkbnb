37b97ad0aaade5734adb2f589461854eecaf569b
yiming <isyiming.zhang@icloud.com>
2022-07-12 11:40:14 +0800

    recode log

--- service/cronjob/monitor/monitor.go
+++ service/cronjob/monitor/monitor.go
@@ -18,94 +18,45 @@ var configFile = flag.String("f",
 
 func main() {
 	flag.Parse()
-
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 	ctx := svc.NewServiceContext(c)
-
 	ZecreyRollupAddress, err := ctx.SysConfigModel.GetSysconfigByName(c.ChainConfig.ZecreyContractAddrSysConfigName)
 	if err != nil {
-		logx.Severef("[monitor] fatal error, cannot fetch ZecreyLegendContractAddr from sysConfig, err: %s, SysConfigName: %s",
-			err.Error(), c.ChainConfig.ZecreyContractAddrSysConfigName)
+		logx.Errorf("[main] GetSysconfigByName err: %s", err)
 		panic(err)
 	}
-
 	NetworkRpc, err := ctx.SysConfigModel.GetSysconfigByName(c.ChainConfig.NetworkRPCSysConfigName)
 	if err != nil {
 		logx.Severef("[monitor] fatal error, cannot fetch NetworkRPC from sysConfig, err: %s, SysConfigName: %s",
 			err.Error(), c.ChainConfig.NetworkRPCSysConfigName)
 		panic(err)
 	}
-
-	logx.Infof("[monitor] ChainName: %s, ZecreyRollupAddress: %s, NetworkRpc: %s",
-		c.ChainConfig.ZecreyContractAddrSysConfigName,
-		ZecreyRollupAddress.Value,
-		NetworkRpc.Value)
-
-	// load client
+	logx.Infof("[monitor] ChainName: %s, ZecreyRollupAddress: %s, NetworkRpc: %s", c.ChainConfig.ZecreyContractAddrSysConfigName, ZecreyRollupAddress.Value, NetworkRpc.Value)
 	zecreyRpcCli, err := _rpc.NewClient(NetworkRpc.Value)
 	if err != nil {
 		panic(err)
 	}
-
-	// new cron
 	cronjob := cron.New(cron.WithChain(
 		cron.SkipIfStillRunning(cron.DiscardLogger),
 	))
-
-	// block monitor
-	_, err = cronjob.AddFunc("@every 10s", func() {
-		logx.Info("========================= start monitor blocks =========================")
-		err := logic.MonitorBlocks(
-			zecreyRpcCli,
-			c.ChainConfig.StartL1BlockHeight, c.ChainConfig.PendingBlocksCount, c.ChainConfig.MaxHandledBlocksCount,
-			ZecreyRollupAddress.Value,
-			ctx.L1BlockMonitorModel,
-		)
-		if err != nil {
-			logx.Error("[logic.MonitorBlocks main] unable to run:", err)
-		}
-		logx.Info("========================= end monitor blocks =========================")
-	})
-	if err != nil {
+	if _, err = cronjob.AddFunc("@every 10s", func() {
+		logic.MonitorBlocks(zecreyRpcCli, c.ChainConfig.StartL1BlockHeight, c.ChainConfig.PendingBlocksCount,
+			c.ChainConfig.MaxHandledBlocksCount, ZecreyRollupAddress.Value, ctx.L1BlockMonitorModel)
+	}); err != nil {
 		panic(err)
 	}
-
-	// mempool monitor
-	_, err = cronjob.AddFunc("@every 15s", func() {
-		logx.Info("===== start monitor mempool txs")
-		err := logic.MonitorMempool(
-			ctx,
-		)
-		if err != nil {
-			if err == logic.ErrNotFound {
-				logx.Info("[logic.MonitorMempool main] no l2 tx event need to monitor")
-			} else {
-				logx.Info("[logic.MonitorMempool main] unable to run:", err)
-			}
-		}
-		logx.Info("===== end monitor mempool txs")
-	})
-
-	// l2 block monitor
-	_, err = cronjob.AddFunc("@every 10s", func() {
-		logx.Info("========================= start l2 monitor blocks =========================")
-		err := logic.MonitorL2BlockEvents(
-			zecreyRpcCli,
-			c.ChainConfig.PendingBlocksCount,
-			ctx.MempoolModel,
-			ctx.BlockModel,
-			ctx.L1TxSenderModel,
-		)
-		if err != nil {
-			logx.Error("[logic.MonitorBlocks main] unable to run:", err)
-		}
-		logx.Info("========================= end l2 monitor blocks =========================")
-	})
-	if err != nil {
+	if _, err = cronjob.AddFunc("@every 10s", func() {
+		logic.MonitorMempool(ctx)
+	}); err != nil {
 		panic(err)
 	}
-
+	if _, err = cronjob.AddFunc("@every 10s", func() {
+		logic.MonitorL2BlockEvents(zecreyRpcCli, c.ChainConfig.PendingBlocksCount,
+			ctx.MempoolModel, ctx.BlockModel, ctx.L1TxSenderModel)
+	}); err != nil {
+		panic(err)
+	}
 	// governance monitor
 	GovernanceContractAddress, err := ctx.SysConfigModel.GetSysconfigByName(c.ChainConfig.GovernanceContractAddrSysConfigName)
 	if err != nil {
@@ -114,24 +65,14 @@ func main() {
 		panic(err)
 	}
 
-	_, err = cronjob.AddFunc("@every 10s", func() {
-		logx.Info("========================= start monitor blocks =========================")
-		err := logic.MonitorGovernanceContract(
-			zecreyRpcCli,
-			c.ChainConfig.StartL1BlockHeight, c.ChainConfig.PendingBlocksCount, c.ChainConfig.MaxHandledBlocksCount,
-			GovernanceContractAddress.Value,
-			ctx.L1BlockMonitorModel,
-			ctx.SysConfigModel,
-			ctx.L2AssetInfoModel,
+	if _, err = cronjob.AddFunc("@every 10s", func() {
+		logic.MonitorGovernanceContract(zecreyRpcCli, c.ChainConfig.StartL1BlockHeight, c.ChainConfig.PendingBlocksCount, c.ChainConfig.MaxHandledBlocksCount,
+			GovernanceContractAddress.Value, ctx.L1BlockMonitorModel, ctx.SysConfigModel, ctx.L2AssetInfoModel,
 		)
-		if err != nil {
-			logx.Error("[logic.MonitorGovernanceContract main] unable to run:", err)
-		}
-		logx.Info("========================= end monitor blocks =========================")
-	})
-
+	}); err != nil {
+		panic(err)
+	}
 	cronjob.Start()
-
 	logx.Info("Starting monitor cronjob ...")
 	select {}
 }
