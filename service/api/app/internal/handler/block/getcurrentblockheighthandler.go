9a8e9faba6d144a892f6aa8e61fb4d358b3e19e0
zhangwei812 <1733773547@qq.com>
2022-07-25 17:08:21 +0800

    add GetCurrentBlockHeight

---
+++ service/api/app/internal/handler/block/getcurrentblockheighthandler.go
@@ -0,0 +1,21 @@
+package block
+
+import (
+	"net/http"
+
+	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/logic/block"
+	"github.com/zecrey-labs/zecrey-legend/service/api/app/internal/svc"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func GetCurrentBlockHeightHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		l := block.NewGetCurrentBlockHeightLogic(r.Context(), svcCtx)
+		resp, err := l.GetCurrentBlockHeight()
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}
