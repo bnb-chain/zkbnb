81b811904820deee3701712a7913d68dd1aa9720
forcodedancing <just.haha.it@gmail.com>
2022-08-09 15:55:48 +0800

    merge app & globalRPC

--- service/api/app/internal/logic/transaction/sendtxlogic.go
+++ service/api/app/internal/logic/transaction/sendtxlogic.go
@@ -35,61 +35,74 @@ func (l *SendTxLogic) SendTx(req *types.ReqSendTx) (resp *types.RespSendTx, err 
 	case commonTx.TxTypeTransfer:
 		resp.TxId, err = sendrawtx.SendTransferTx(l.ctx, l.svcCtx, l.commglobalmap, req.TxInfo)
 		if err != nil {
-			logx.Errorf("[sendTransferTx] err: %s", err.Error())
+			logx.Errorf("sendTransferTx err: %s", err.Error())
 			return nil, err
 		}
 	case commonTx.TxTypeSwap:
 		resp.TxId, err = sendrawtx.SendSwapTx(l.ctx, l.svcCtx, l.commglobalmap, req.TxInfo)
 		if err != nil {
-			logx.Errorf("[sendSwapTx] err: %s", err.Error())
+			logx.Errorf("sendSwapTx err: %s", err.Error())
 			return nil, err
 		}
 	case commonTx.TxTypeAddLiquidity:
 		resp.TxId, err = sendrawtx.SendAddLiquidityTx(l.ctx, l.svcCtx, l.commglobalmap, req.TxInfo)
 		if err != nil {
-			logx.Errorf("[sendAddLiquidityTx] err: %s", err.Error())
+			logx.Errorf("sendAddLiquidityTx err: %s", err.Error())
 			return nil, err
 		}
 	case commonTx.TxTypeRemoveLiquidity:
 		resp.TxId, err = sendrawtx.SendRemoveLiquidityTx(l.ctx, l.svcCtx, l.commglobalmap, req.TxInfo)
 		if err != nil {
-			logx.Errorf("[sendRemoveLiquidityTx] err: %s", err.Error())
+			logx.Errorf("sendRemoveLiquidityTx err: %s", err.Error())
 			return nil, err
 		}
 	case commonTx.TxTypeWithdraw:
 		resp.TxId, err = sendrawtx.SendWithdrawTx(l.ctx, l.svcCtx, l.commglobalmap, req.TxInfo)
 		if err != nil {
-			logx.Errorf("[sendWithdrawTx] err: %s", err.Error())
+			logx.Errorf("sendWithdrawTx err: %s", err.Error())
 			return nil, err
 		}
 	case commonTx.TxTypeTransferNft:
 		resp.TxId, err = sendrawtx.SendTransferNftTx(l.ctx, l.svcCtx, l.commglobalmap, req.TxInfo)
 		if err != nil {
-			logx.Errorf("[sendWithdrawTx] err: %s", err.Error())
+			logx.Errorf("sendTransferNftTX err: %s", err.Error())
 			return nil, err
 		}
 	case commonTx.TxTypeAtomicMatch:
 		resp.TxId, err = sendrawtx.SendAtomicMatchTx(l.ctx, l.svcCtx, l.commglobalmap, req.TxInfo)
 		if err != nil {
-			logx.Errorf("[sendWithdrawTx] err: %s", err.Error())
+			logx.Errorf("sendAtomicMatchTx err: %s", err.Error())
 			return nil, err
 		}
 	case commonTx.TxTypeCancelOffer:
 		resp.TxId, err = sendrawtx.SendCancelOfferTx(l.ctx, l.svcCtx, l.commglobalmap, req.TxInfo)
 		if err != nil {
-			logx.Errorf("[sendWithdrawTx] err: %s", err.Error())
+			logx.Errorf("sendCancelOfferTx err: %s", err.Error())
 			return nil, err
 		}
 	case commonTx.TxTypeWithdrawNft:
 		resp.TxId, err = sendrawtx.SendWithdrawNftTx(l.ctx, l.svcCtx, l.commglobalmap, req.TxInfo)
 		if err != nil {
-			logx.Errorf("[sendWithdrawTx] err: %s", err.Error())
+			logx.Errorf("sendWithdrawNftTx err: %s", err.Error())
+			return nil, err
+		}
+	case commonTx.TxTypeCreatePair:
+		resp.TxId, err = sendrawtx.SendCreateCollectionTx(l.ctx, l.svcCtx, l.commglobalmap, req.TxInfo)
+		if err != nil {
+			logx.Errorf("sendCreateCollectionTx err: %s", err.Error())
+			return nil, err
+		}
+	case commonTx.TxTypeMintNft:
+		resp.TxId, err = sendrawtx.SendMintNftTx(l.ctx, l.svcCtx, l.commglobalmap, req.TxInfo)
+		if err != nil {
+			logx.Errorf("sendMintNftTx err: %s", err.Error())
 			return nil, err
 		}
 	case commonTx.TxTypeOffer:
+		logx.Errorf("invalid tx type: %s", req.TxType)
 		return nil, errorcode.AppErrInvalidTxType
 	default:
-		logx.Errorf("[sendtxlogic] invalid tx type: %s", req.TxType)
+		logx.Errorf("invalid tx type: %s", req.TxType)
 		return nil, errorcode.AppErrInvalidTxType
 	}
 	return resp, err
