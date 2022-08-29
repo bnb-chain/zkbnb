6f56d53ee16e7355305bf6db607b9a9c1c545452
WayToFuture <just.haha.it@gmail.com>
2022-08-29 15:58:50 +0800

    fix: fix cache interface and usage (#111)

--- dao/dbcache/cache.go
+++ dao/dbcache/cache.go
@@ -9,7 +9,7 @@ type QueryFunc func() (interface{}, error)
 
 type Cache interface {
 	GetWithSet(ctx context.Context, key string, value interface{}, query QueryFunc) (interface{}, error)
-	Get(ctx context.Context, key string) (interface{}, error)
+	Get(ctx context.Context, key string, value interface{}) (interface{}, error)
 	Set(ctx context.Context, key string, value interface{}) error
 	Delete(ctx context.Context, key string) error
 }
