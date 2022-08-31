d076efcf744483a9bd65d4a7b10e86ffacdd68a6
WayToFuture <just.haha.it@gmail.com>
2022-08-31 13:49:58 +0800

    apiserver: enable cors for apis (#130)

--- service/apiserver/server.go
+++ service/apiserver/server.go
@@ -20,7 +20,7 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
-	server := rest.MustNewServer(c.RestConf)
+	server := rest.MustNewServer(c.RestConf, rest.WithCors())
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
