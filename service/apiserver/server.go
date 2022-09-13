068b88bd559637511fcfaa8af2728c0b1190d348
forcodedancing <just.haha.it@gmail.com>
2022-09-13 15:14:19 +0800

    graceful shutdown for apiserver

--- service/apiserver/server.go
+++ service/apiserver/server.go
@@ -1,14 +1,19 @@
 package apiserver
 
 import (
+	"time"
+
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/proc"
 	"github.com/zeromicro/go-zero/rest"
 
 	"github.com/bnb-chain/zkbnb/service/apiserver/internal/config"
 	"github.com/bnb-chain/zkbnb/service/apiserver/internal/handler"
 	"github.com/bnb-chain/zkbnb/service/apiserver/internal/svc"
 )
+
+const GracefulShutdownTimeout = 5 * time.Second
 
 func Run(configFile string) error {
 	var c config.Config
@@ -17,12 +22,17 @@ func Run(configFile string) error {
 	logx.DisableStat()
 
 	server := rest.MustNewServer(c.RestConf, rest.WithCors())
-	defer server.Stop()
-
 	ctx := svc.NewServiceContext(c)
 	handler.RegisterHandlers(server, ctx)
 
-	logx.Infof("Starting server at %s:%d...\n", c.Host, c.Port)
+	proc.SetTimeToForceQuit(GracefulShutdownTimeout)
+	proc.AddShutdownListener(func() {
+		logx.Info("start to shutdown apiserver......")
+		server.Stop()
+		ctx.Shutdown()
+	})
+
+	logx.Infof("apiserver is starting at %s:%d...\n", c.Host, c.Port)
 	server.Start()
 	return nil
 }
