dfe58ccd2cd3754c964e5568efa63e9728f7dea6
carlcarl <carl.h@binance.com>
2022-10-07 18:54:06 +0800

    draft of error mapping

--- core/block_processor.go
+++ core/block_processor.go
@@ -3,6 +3,8 @@ package core
 import (
 	"fmt"
 
+	"github.com/bnb-chain/zkbnb-crypto/wasm/txtypes"
+	"github.com/pkg/errors"
 	"github.com/zeromicro/go-zero/core/logx"
 
 	"github.com/bnb-chain/zkbnb/core/executor"
@@ -87,8 +89,73 @@ func (p *APIProcessor) Process(tx *tx.Tx) error {
 	}
 	err = executor.VerifyInputs(false)
 	if err != nil {
-		return types.AppErrInvalidTxField.RefineError(err.Error())
+		return mappingAPIErrors(err)
 	}
 
 	return nil
+}
+
+func mappingAPIErrors(err error) error {
+	e := errors.Cause(err)
+	switch e {
+	case txtypes.ErrAccountIndexTooLow, txtypes.ErrAccountIndexTooHigh,
+		txtypes.ErrCreatorAccountIndexTooLow, txtypes.ErrCreatorAccountIndexTooHigh,
+		txtypes.ErrFromAccountIndexTooLow, txtypes.ErrFromAccountIndexTooHigh,
+		txtypes.ErrToAccountIndexTooLow, txtypes.ErrToAccountIndexTooHigh:
+		return types.AppErrTxInvalidAccountIndex
+	case txtypes.ErrGasAccountIndexTooLow, txtypes.ErrGasAccountIndexTooHigh:
+		return types.AppErrTxInvalidGasFeeAccount
+	case txtypes.ErrGasFeeAssetIdTooLow, txtypes.ErrGasFeeAssetIdTooHigh:
+		return types.AppErrTxInvalidGasFeeAsset
+	case txtypes.ErrGasFeeAssetAmountTooLow, txtypes.ErrGasFeeAssetAmountTooHigh:
+		return types.AppErrTxInvalidGasFeeAmount
+	case txtypes.ErrNonceTooLow:
+		return types.AppErrTxInvalidNonce
+	case txtypes.ErrOfferTypeInvalid:
+		return types.AppErrTxInvalidOfferType
+	case txtypes.ErrOfferIdTooLow:
+		return types.AppErrTxInvalidOfferId
+	case txtypes.ErrNftIndexTooLow:
+		return types.AppErrTxInvalidNftIndex
+	case txtypes.ErrAssetIdTooLow, txtypes.ErrAssetIdTooHigh,
+		txtypes.ErrAssetAIdTooLow, txtypes.ErrAssetAIdTooHigh:
+		return types.AppErrTxInvalidAssetId
+	case txtypes.ErrAssetAmountTooLow, txtypes.ErrAssetAmountTooHigh,
+		txtypes.ErrAssetAAmountTooLow, txtypes.ErrAssetAAmountTooHigh,
+		txtypes.ErrAssetBAmountTooLow, txtypes.ErrAssetBAmountTooHigh:
+		return types.AppErrTxInvalidAssetAmount
+	case txtypes.ErrListedAtTooLow:
+		return types.AppErrTxInvalidListTime
+	case txtypes.ErrTreasuryRateTooLow, txtypes.ErrTreasuryRateTooHigh,
+		txtypes.ErrCreatorTreasuryRateTooLow, txtypes.ErrCreatorTreasuryRateTooHigh:
+		return types.AppErrTxInvalidTreasuryRate
+	case txtypes.ErrCollectionNameTooShort, txtypes.ErrCollectionNameTooLong:
+		return types.AppErrTxInvalidCollectionName
+	case txtypes.ErrIntroductionTooLong:
+		return types.AppErrTxInvalidIntroduction
+	case txtypes.ErrNftContentHashInvalid:
+		return types.AppErrTxInvalidNftContenthash
+	case txtypes.ErrNftCollectionIdTooLow, txtypes.ErrNftCollectionIdTooHigh:
+		return types.AppErrTxInvalidCollectionId
+	case txtypes.ErrPairIndexTooLow, txtypes.ErrPairIndexTooHigh:
+		return types.AppErrTxInvalidPairIndex
+	case txtypes.ErrLpAmountTooLow, txtypes.ErrLpAmountTooLow:
+		return types.AppErrTxInvalidLpAmount
+	case txtypes.ErrCallDataHashInvalid:
+		return types.AppErrTxInvalidCallDataHash
+	case txtypes.ErrToAccountNameHashInvalid:
+		return types.AppErrTxInvalidToAccountNameHash
+	case txtypes.ErrAssetAMinAmountTooLow, txtypes.ErrAssetAMinAmountTooHigh,
+		txtypes.ErrAssetBMinAmountTooLow, txtypes.ErrAssetBMinAmountTooHigh:
+		return types.AppErrTxInvalidAssetMinAmount
+	case txtypes.ErrToAddressInvalid:
+		return types.AppErrTxInvalidToAddress
+	case txtypes.ErrBuyOfferInvalid:
+		return types.AppErrTxInvalidBuyOffer
+	case txtypes.ErrSellOfferInvalid:
+		return types.AppErrTxInvalidSellOffer
+
+	default:
+		return types.AppErrInvalidTxField.RefineError(err.Error())
+	}
 }
