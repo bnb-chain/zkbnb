781774bd768e142f0f92c782ee35ade9d8ce6f8d
Carl Huang <carlcarlking@gmail.com>
2022-10-18 16:42:46 +0800

    feat: export toAccountIndex and toAccountName in Tx APIs (#226)
    Co-authored-by: carlcarl <carl.h@binance.com>

--- service/apiserver/internal/logic/transaction/gettxlogic.go
+++ service/apiserver/internal/logic/transaction/gettxlogic.go
@@ -34,6 +34,9 @@ func (l *GetTxLogic) GetTx(req *types.ReqGetTx) (resp *types.EnrichedTx, err err
 		resp.Tx = *utils.ConvertTx(tx)
 		resp.Tx.AccountName, _ = l.svcCtx.MemCache.GetAccountNameByIndex(tx.AccountIndex)
 		resp.Tx.AssetName, _ = l.svcCtx.MemCache.GetAssetNameById(tx.AssetId)
+		if resp.Tx.ToAccountIndex >= 0 {
+			resp.Tx.ToAccountName, _ = l.svcCtx.MemCache.GetAccountNameByIndex(resp.Tx.ToAccountIndex)
+		}
 		block, err := l.svcCtx.MemCache.GetBlockByHeightWithFallback(tx.BlockHeight, func() (interface{}, error) {
 			return l.svcCtx.BlockModel.GetBlockByHeight(resp.Tx.BlockHeight)
 		})
@@ -56,6 +59,9 @@ func (l *GetTxLogic) GetTx(req *types.ReqGetTx) (resp *types.EnrichedTx, err err
 		resp.Tx = *utils.ConvertTx(poolTx)
 		resp.Tx.AccountName, _ = l.svcCtx.MemCache.GetAccountNameByIndex(poolTx.AccountIndex)
 		resp.Tx.AssetName, _ = l.svcCtx.MemCache.GetAssetNameById(poolTx.AssetId)
+		if resp.Tx.ToAccountIndex >= 0 {
+			resp.Tx.ToAccountName, _ = l.svcCtx.MemCache.GetAccountNameByIndex(resp.Tx.ToAccountIndex)
+		}
 	}
 
 	return resp, nil
