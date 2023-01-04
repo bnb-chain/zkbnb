e1b8633183dc70bec54e6deda46adbbeb97a061c
toni.x <toni.x@binance.com>
2023-01-04 16:49:08 +0300

    add asset icon

--- service/apiserver/internal/logic/asset/getassetslogic.go
+++ service/apiserver/internal/logic/asset/getassetslogic.go
@@ -62,7 +62,7 @@ func (l *GetAssetsLogic) GetAssets(req *types.ReqGetRange) (resp *types.Assets, 
 			Address:    asset.L1Address,
 			Price:      strconv.FormatFloat(assetPrice, 'E', -1, 64),
 			IsGasAsset: asset.IsGasAsset,
-			Icon:       fmt.Sprintf(iconBaseUrl, strings.ToLower(asset.AssetSymbol), strings.ToLower(asset.AssetSymbol)),
+			Icon:       fmt.Sprintf(iconBaseUrl, strings.ToLower(asset.AssetSymbol)),
 		})
 	}
 	return resp, nil
