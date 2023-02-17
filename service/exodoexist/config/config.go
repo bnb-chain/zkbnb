34c5a2413732a804e4f0d4f2082c94b603193e77
markc <mark.c@binance.com>
2023-02-17 17:56:10 +0300

    exodo exist

---
+++ service/exodoexist/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"github.com/zeromicro/go-zero/core/logx"
+
+	"github.com/bnb-chain/zkbnb/tree"
+)
+
+type Config struct {
+	Postgres struct {
+		MasterDataSource string
+		SlaveDataSource  string
+	}
+	TreeDB struct {
+		Driver tree.Driver
+		//nolint:staticcheck
+		LevelDBOption tree.LevelDBOption `json:",optional"`
+		//nolint:staticcheck
+		RedisDBOption tree.RedisDBOption `json:",optional"`
+		//nolint:staticcheck
+		RoutinePoolSize    int `json:",optional"`
+		AssetTreeCacheSize int
+	}
+	LogConf logx.LogConf
+}
---
+++ service/exodoexist/exodoexist.go
@@ -0,0 +1,60 @@
+package exodoexist
+
+import (
+	"github.com/bnb-chain/zkbnb/service/witness/witness"
+	"time"
+
+	"github.com/robfig/cron/v3"
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/proc"
+
+	"github.com/bnb-chain/zkbnb/service/exodoexist/config"
+)
+
+const GracefulShutdownTimeout = 5 * time.Second
+
+func Run(configFile string) error {
+	var c config.Config
+	conf.MustLoad(configFile, &c)
+	logx.MustSetup(c.LogConf)
+	logx.DisableStat()
+
+	w, err := witness.NewWitness(c)
+	if err != nil {
+		logx.Severe(err)
+		panic(err)
+	}
+	cronJob := cron.New(cron.WithChain(
+		cron.SkipIfStillRunning(cron.DiscardLogger),
+	))
+	_, err = cronJob.AddFunc("@every 2s", func() {
+		logx.Info("==========start generate block witness==========")
+		err := w.GenerateBlockWitness()
+		if err != nil {
+			logx.Severef("failed to generate block witness, %v", err)
+			panic(err)
+		}
+		w.RescheduleBlockWitness()
+	})
+	if err != nil {
+		logx.Severe(err)
+		panic(err)
+	}
+	cronJob.Start()
+
+	exit := make(chan struct{})
+	proc.SetTimeToForceQuit(GracefulShutdownTimeout)
+	proc.AddShutdownListener(func() {
+		logx.Info("start to shutdown witness......")
+		<-cronJob.Stop().Done()
+		w.Shutdown()
+		_ = logx.Close()
+		exit <- struct{}{}
+	})
+
+	logx.Info("witness cronjob is starting......")
+
+	<-exit
+	return nil
+}
