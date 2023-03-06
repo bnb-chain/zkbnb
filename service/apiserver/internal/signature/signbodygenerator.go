1432663e81f10a89fb1fc1e7311b57aed52002cb
Smith S <smith.s@binance.com>
2023-03-06 11:09:23 +0300

    Optimization Signature Body Generating Template

--- service/apiserver/internal/signature/signbodygenerator.go
+++ service/apiserver/internal/signature/signbodygenerator.go
@@ -11,22 +11,22 @@ import (
 
 const (
 
-	// SignatureTemplateWithdrawal Withdrawal ${amount} to: ${to.toLowerCase()}\nFee: ${fee} ${feeTokenAddress}\nNonce: ${nonce}
-	SignatureTemplateWithdrawal = "Withdrawal %s to: %s\nFee: %s %d\nNonce: %d"
-	// SignatureTemplateTransfer /* Transfer ${amount} ${tokenAddress} to: ${to.toLowerCase()}\nFee: ${fee} ${feeTokenAddress}\nNonce: ${nonce} */
-	SignatureTemplateTransfer = "Transfer %s %d to: %d\nFee: %s %d\nNonce: %d"
-	// SignatureTemplateCreateCollection CreateCollection ${accountIndex} ${collectionName} \nFee: ${fee} ${feeTokenAddress}\nNonce: ${nonce}
-	SignatureTemplateCreateCollection = "CreateCollection %d %s \nFee: %s %d\nNonce: %d"
-	// SignatureTemplateMintNft MintNFT ${contentHash} for: ${recipient.toLowerCase()}\nFee: ${fee} ${feeTokenAddress}\nNonce: ${nonce}
-	SignatureTemplateMintNft = "MintNFT %s for: %d\nFee: %s %d\nNonce: %d"
-	// SignatureTemplateTransferNft TransferNFT ${NftIndex} ${fromAccountIndex} to ${toAccountIndex} \nFee: ${fee} ${feeTokenAddress}\nNonce: ${nonce}
-	SignatureTemplateTransferNft = "TransferNFT %d %d to %d \nFee: %s %d\nNonce: %d"
-	// SignatureTemplateWithdrawalNft Withdrawal ${tokenIndex} to: ${to.toLowerCase()}\nFee: ${fee} ${feeTokenAddress}\nNonce: ${nonce}
-	SignatureTemplateWithdrawalNft = "Withdrawal %d to: %s\nFee: %s %d\nNonce: %d"
-	// SignatureTemplateCancelOffer CancelOffer ${offerId} by: ${accountIndex} \nFee: ${fee} ${feeTokenAddress}\nNonce: ${nonce}
-	SignatureTemplateCancelOffer = "CancelOffer %d by: %d \nFee: %s %d\nNonce: %d"
-	// SignatureTemplateAtomicMatch AtomicMatch ${amount} ${offerId} ${nftIndex} ${accountIndex} \nFee: ${fee} ${feeTokenAddress}\nNonce: ${nonce}
-	SignatureTemplateAtomicMatch = "AtomicMatch %s %d %d %d \nFee: %s %d\nNonce: %d"
+	// SignatureTemplateWithdrawal Withdrawal ${amount} to: ${to.toLowerCase()}\nGasFee: ${fee} \nGasAccountIndex: ${feeTokenAddress}\nNonce: ${nonce}
+	SignatureTemplateWithdrawal = "Withdrawal %s to: %s\nGasFee: %s \nGasAccountIndex: %d\nNonce: %d"
+	// SignatureTemplateTransfer /* Transfer ${amount} ${tokenAddress} to: ${to.toLowerCase()}\nGasFee: ${fee} \nGasAccountIndex: ${feeTokenAddress}\nNonce: ${nonce} */
+	SignatureTemplateTransfer = "Transfer %s %d to: %d\nGasFee: %s \nGasAccountIndex: %d\nNonce: %d"
+	// SignatureTemplateCreateCollection CreateCollection ${accountIndex} ${collectionName} \nGasFee: ${fee} \nGasAccountIndex: ${feeTokenAddress}\nNonce: ${nonce}
+	SignatureTemplateCreateCollection = "CreateCollection %d %s \nGasFee: %s \nGasAccountIndex: %d\nNonce: %d"
+	// SignatureTemplateMintNft MintNFT ${contentHash} for: ${recipient.toLowerCase()}\nGasFee: ${fee} \nGasAccountIndex: ${feeTokenAddress}\nNonce: ${nonce}
+	SignatureTemplateMintNft = "MintNFT %s for: %d\nGasFee: %s \nGasAccountIndex: %d\nNonce: %d"
+	// SignatureTemplateTransferNft TransferNFT ${NftIndex} ${fromAccountIndex} to ${toAccountIndex} \nGasFee: ${fee} \nGasAccountIndex: ${feeTokenAddress}\nNonce: ${nonce}
+	SignatureTemplateTransferNft = "TransferNFT %d %d to %d \nGasFee: %s \nGasAccountIndex: %d\nNonce: %d"
+	// SignatureTemplateWithdrawalNft Withdrawal ${tokenIndex} to: ${to.toLowerCase()}\nGasFee: ${fee} \nGasAccountIndex: ${feeTokenAddress}\nNonce: ${nonce}
+	SignatureTemplateWithdrawalNft = "Withdrawal %d to: %s\nGasFee: %s \nGasAccountIndex: %d\nNonce: %d"
+	// SignatureTemplateCancelOffer CancelOffer ${offerId} by: ${accountIndex} \nGasFee: ${fee} \nGasAccountIndex: ${feeTokenAddress}\nNonce: ${nonce}
+	SignatureTemplateCancelOffer = "CancelOffer %d by: %d \nGasFee: %s \nGasAccountIndex: %d\nNonce: %d"
+	// SignatureTemplateAtomicMatch AtomicMatch ${amount} ${offerId} ${nftIndex} ${accountIndex} \nGasFee: ${fee} \nGasAccountIndex: ${feeTokenAddress}\nNonce: ${nonce}
+	SignatureTemplateAtomicMatch = "AtomicMatch %s %d %d %d \nGasFee: %s \nGasAccountIndex: %d\nNonce: %d"
 
 	// SignatureTemplateAccount AtomicMatch %{account}
 	SignatureTemplateAccount = "TxTypeEmpty %d"
