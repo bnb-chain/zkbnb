f9d05739ff667a881ab9112f19299bf4669630a2
Damon <tango.qct@gmail.com>
2023-04-28 15:57:26 +0400

    Polish Generate verifier from vk

--- deployment/prod/generate_verifier.sh
+++ deployment/prod/generate_verifier.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Generate verifier contract"
-
 # Import! set correct block sizes & key path
-BLOCK_SIZES=1,2
+OPTIONAL_BLOCK_SIZES=${BLOCK_SIZES:-"1,2"}
 KEY_PATH=~/zkbnb-deploy/zkbnb-crypto/circuit/solidity
 TEMPLATE="https://raw.githubusercontent.com/bnb-chain/zkbnb-contract/qa/contracts/ZkBNBVerifier.sol"
+
+echo "Generate verifier contract for block sizes: ${OPTIONAL_BLOCK_SIZES}"
 
 BASE_PATH=$(cd `dirname $0`/../..; pwd)
 cd ${BASE_PATH}/service/prover
@@ -13,7 +13,7 @@ cd ${BASE_PATH}/service/prover
 contracts=()
 keys=()
 i=0
-for size in $(echo $BLOCK_SIZES | tr ',' ' '); do
+for size in $(echo $OPTIONAL_BLOCK_SIZES | tr ',' ' '); do
  contracts[$i]="${KEY_PATH}/ZkBNBVerifier${size}.sol"
  keys[$i]="${KEY_PATH}/zkbnb${size}"
  i=$((i+1))
@@ -22,5 +22,6 @@ VERIFIER_CONTRACTS=$(echo "${contracts[*]}" | tr ' ' ',')
 PROVING_KEYS=$(echo "${keys[*]}" | tr ' ' ',')
 
 curl -L $TEMPLATE -O $BASE_PATH/ZkBNBVerifier.sol
-python3 verifier_parse.py ${VERIFIER_CONTRACTS} ${BLOCK_SIZES} ZkBNBVerifier.sol
+python3 verifier_parse.py ${VERIFIER_CONTRACTS} ${OPTIONAL_BLOCK_SIZES} ZkBNBVerifier.sol
 rm -rf $BASE_PATH/ZkBNBVerifier.sol
+cp ZkBNBVerifier.sol $BASE_PATH/ZkBNBVerifier.sol
